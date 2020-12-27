137d9c61f6d9f5d63c4687849e1f24c87f614290
Waikar, Bharat <waikarbs@yahoo.com>
2020-12-27 11:43:36 +0530

    First Commit

---
+++ src/main/java/com/BW/JenkinGITMvnDocker/Greeter.java
@@ -0,0 +1,11 @@
+package com.BW.JenkinGITMvnDocker;
+public class Greeter 
+{
+	public Greeter() {
+		
+	}
+    public String greet(String someone) {
+    	return String.format("hello, %s!",  someone);
+    	
+    }
+}
---
+++ src/test/java/com/BW/JenkinGITMvnDocker/TestGreeter.java
@@ -0,0 +1,40 @@
+package com.BW.JenkinGITMvnDocker;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import org.junit.Assert;
+
+
+public class TestGreeter     
+{
+    private Greeter greeter;
+    
+    @Before
+    public void setup() {
+    	greeter = new Greeter();
+    }
+    @Test
+    public void greetShouldIncludeTheOneBeingGreeted(){
+    	String someone ="World";
+    	Assert.assertNotEquals(greeter.greet(someone), (someone));
+    	//asertThat(greeter.greet(someone), containsString(someone));
+    	
+    	
+    }
+   
+    @Test
+    public void greetShouldIncludeGreetingPhrase(){
+    	String someone ="World";
+    	Assert.assertEquals(greeter.greet(someone).length(), 13);
+    	
+    }
+    
+    @Test
+    public void greetShouldIncludeGreetingPhrase1(){
+    	String someone ="World";
+    	Assert.assertEquals(greeter.greet(someone).length(), 13);
+    	
+    }
+    
+}
